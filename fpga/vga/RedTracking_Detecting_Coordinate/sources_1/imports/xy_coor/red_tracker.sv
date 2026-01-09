1eed2d7be39e0ede11d76a5e0f385b8559e2ec4d
kkm1029 <rhkdals6933@gmail.com>
2026-01-09 21:35:20 +0900

    0109_2134_Multi__Red_Detect

--- fpga/vga/RedTracking_Detecting_Coordinate/sources_1/imports/xy_coor/red_tracker.sv
+++ fpga/vga/RedTracking_Detecting_Coordinate/sources_1/imports/xy_coor/red_tracker.sv
@@ -8,131 +8,126 @@ module red_tracker (
     input  logic [ 9:0] x_pixel,
     input  logic [ 9:0] y_pixel,
     input  logic [15:0] data,
-    output logic [ 9:0] aim_x,
-    output logic [ 9:0] aim_y,
-    output logic        aim_detected,
-    output logic        raser_shoot,
-    output logic [11:0] x_min_out,
-    output logic [11:0] x_max_out,
-    output logic [11:0] y_min_out,
-    output logic [11:0] y_max_out,
+    
+    // --- [출력] 16개의 타겟 좌표 (배열) ---
+    output logic [15:0][9:0] aim_x_all, 
+    output logic [15:0][9:0] aim_y_all,
+    output logic [15:0]      aim_detected_all,
+    
+    // --- [출력] 16개의 바운딩 박스 좌표 (배열) ---
+    output logic [15:0][11:0] x_min_all,
+    output logic [15:0][11:0] x_max_all,
+    output logic [15:0][11:0] y_min_all,
+    output logic [15:0][11:0] y_max_all,
+    
     output logic        target_off
 );
 
     localparam SEC_3 = 75_000_000;
     logic [$clog2(SEC_3) - 1 : 0] target_cnt;
 
+    // RGB 추출
     logic [4:0] r_val;
     logic [5:0] g_val;
     logic [4:0] b_val;
-
     assign r_val = data[15:11];
     assign g_val = data[10:5];
     assign b_val = data[4:0];
 
+    // 빨간색 판별 조건
     logic is_red;
-    // assign is_red = (r_val > 5'd20) && (g_val < 6'd15) && (b_val < 5'd15); // 15
-    assign is_red = (r_val > 5'd18) &&    
-        (g_val < 6'd15) &&         
-        (b_val < 5'd15) && 
-        (r_val > g_val) &&         
-        (r_val > b_val);   
+    assign is_red = (r_val > 5'd20) && (g_val < 6'd15) && (b_val < 5'd15);
 
-    logic [11:0] x_min, x_max;
-    logic [11:0] y_min, y_max;
-    logic [16:0] pixel_count;
+    // --- 16개 구역 내부 변수 ---
+    logic [11:0] x_min[15:0], x_max[15:0], y_min[15:0], y_max[15:0];
+    logic [16:0] p_count[15:0];
 
-    // Raser Shoot Signal
-    logic is_taken_aimX;
-    assign is_taken_aimX = (aim_x >= 256+32 && aim_x <= 383-32) ? 1'b1 : 1'b0;
-    logic is_taken_aimY;
-    assign is_taken_aimY = (aim_y >= 192+16 && aim_y <= 287-16) ? 1'b1 : 1'b0;
-    assign raser_shoot = (aim_detected & is_taken_aimX & is_taken_aimY);
-
-    logic vsync_d;
-    logic vsync_start;
-
-    logic [9:0] center_x, center_y;
-    logic [9:0] diff_x, diff_y;
-
-    always_ff @(posedge clk) begin
-        vsync_d <= v_sync;
-    end
-
+    logic vsync_d, vsync_start;
+    always_ff @(posedge clk) vsync_d <= v_sync;
     assign vsync_start = v_sync && !vsync_d;
 
+    // 구역 인덱스 계산 (0~15)
+    logic [3:0] region_idx;
+    logic [1:0] col, row;
+
+    always_comb begin
+        if      (x_pixel < 160) col = 0;
+        else if (x_pixel < 320) col = 1;
+        else if (x_pixel < 480) col = 2;
+        else                    col = 3;
+
+        if      (y_pixel < 120) row = 0;
+        else if (y_pixel < 240) row = 1;
+        else if (y_pixel < 360) row = 2;
+        else                    row = 3;
+        
+        region_idx = {row, col};
+    end
+
+    integer i;
+    
     always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
-            aim_x <= 0;
-            aim_y <= 0;
-            aim_detected <= 0;
-            x_min <= 12'd4095;
-            x_max <= 0;
-            y_min <= 12'd4095;
-            y_max <= 0;
-            pixel_count <= 0;
-            x_min_out <= 0;
-            x_max_out <= 0;
-            y_min_out <= 0;
-            y_max_out <= 0;
+            aim_x_all <= 0; aim_y_all <= 0; aim_detected_all <= 0;
+            target_cnt <= 0; target_off <= 0;
+            
+            for(i=0; i<16; i=i+1) begin
+                x_min_all[i] <= 0; x_max_all[i] <= 0;
+                y_min_all[i] <= 0; y_max_all[i] <= 0;
+                
+                x_min[i] <= 12'd4095; x_max[i] <= 0;
+                y_min[i] <= 12'd4095; y_max[i] <= 0;
+                p_count[i] <= 0;
+            end
+
         end else begin
             if (vsync_start) begin
-                if (pixel_count > 50) begin // 임계치 이상으로 Red를 발견했을 때, X/Y 좌표 측정
-                    aim_detected <= 1'b1;
-
-                    center_x = (x_min + x_max) >> 1;
-                    center_y = (y_min + y_max) >> 1;
-
-                    diff_x = (center_x > aim_x) ? (center_x - aim_x) : (aim_x - center_x);
-                    diff_y = (center_y > aim_y) ? (center_y - aim_y) : (aim_y - center_y);
-
-                    if (diff_x > 10) aim_x <= center_x;
-                    if (diff_y > 10) aim_y <= center_y;
-
-                    x_min_out <= x_min;
-                    x_max_out <= x_max;
-                    y_min_out <= y_min;
-                    y_max_out <= y_max;
-                end else begin
-                    aim_detected <= 1'b0;
+                // --- 프레임 종료: 좌표 업데이트 ---
+                for(i=0; i<16; i=i+1) begin
+                    if (p_count[i] > 30) begin // 노이즈 필터
+                        aim_detected_all[i] <= 1'b1;
+                        aim_x_all[i] <= (x_min[i] + x_max[i]) >> 1;
+                        aim_y_all[i] <= (y_min[i] + y_max[i]) >> 1;
+                        
+                        x_min_all[i] <= x_min[i];
+                        x_max_all[i] <= x_max[i];
+                        y_min_all[i] <= y_min[i];
+                        y_max_all[i] <= y_max[i];
+                    end else begin
+                        aim_detected_all[i] <= 1'b0;
+                        x_min_all[i] <= 0; x_max_all[i] <= 0;
+                        y_min_all[i] <= 0; y_max_all[i] <= 0;
+                    end
                 end
 
-                x_min <= 12'd4095;
-                x_max <= 0;
-                y_min <= 12'd4095;
-                y_max <= 0;
-                pixel_count <= 0;
+                // --- Target Off 타이머 ---
+                if (aim_detected_all != 0) begin
+                    target_cnt <= 0;
+                    target_off <= 0;
+                end else begin
+                    if (target_cnt < SEC_3) target_cnt <= target_cnt + 1;
+                    else target_off <= 1'b1;
+                end
+
+                // --- 변수 리셋 ---
+                for(i=0; i<16; i=i+1) begin
+                    x_min[i] <= 12'd4095; x_max[i] <= 0;
+                    y_min[i] <= 12'd4095; y_max[i] <= 0;
+                    p_count[i] <= 0;
+                end
 
             end else begin
-                if (DE && is_red && (x_pixel > 10) && (x_pixel < 630)) begin
-                    pixel_count <= pixel_count + 1;
-
-                    if ({2'b00, x_pixel} < x_min) x_min <= {2'b00, x_pixel};
-                    if ({2'b00, x_pixel} > x_max) x_max <= {2'b00, x_pixel};
-
-                    if ({2'b00, y_pixel} < y_min) y_min <= {2'b00, y_pixel};
-                    if ({2'b00, y_pixel} > y_max) y_max <= {2'b00, y_pixel};
+                // --- 픽셀 스캔 ---
+                if (DE && is_red) begin
+                    p_count[region_idx] <= p_count[region_idx] + 1;
+                    
+                    if ({2'b00, x_pixel} < x_min[region_idx]) x_min[region_idx] <= {2'b00, x_pixel};
+                    if ({2'b00, x_pixel} > x_max[region_idx]) x_max[region_idx] <= {2'b00, x_pixel};
+                    
+                    if ({2'b00, y_pixel} < y_min[region_idx]) y_min[region_idx] <= {2'b00, y_pixel};
+                    if ({2'b00, y_pixel} > y_max[region_idx]) y_max[region_idx] <= {2'b00, y_pixel};
                 end
             end
         end
     end
-
-    always_ff @(posedge clk or posedge reset) begin
-        if (reset) begin
-            target_cnt <= 0;
-            target_off  <= 0;
-        end else begin
-            if (aim_detected) begin
-                target_cnt <= 0;
-                target_off  <= 0;
-            end else begin
-                if (target_cnt < SEC_3) begin
-                    target_cnt <= target_cnt + 1;
-                end else begin
-                    target_off <= 1'b1;
-                end
-            end
-        end
-    end
-
 endmodule
--- fpga/vga/RedTracking_Detecting_Coordinate/sources_1/imports/xy_coor/top.sv
+++ fpga/vga/RedTracking_Detecting_Coordinate/sources_1/imports/xy_coor/top.sv
@@ -1,146 +1,76 @@
 `timescale 1ns / 1ps
 
 module top (
-    // global side
     input  logic       clk,
     input  logic       reset,
-    // OV7670 side
     output logic       xclk,
     input  logic       pclk,
     input  logic       href,
     input  logic       vsync,
     input  logic [7:0] data,
-    // VGA side
     output logic       h_sync,
     output logic       v_sync,
     output logic [3:0] r_port,
     output logic [3:0] g_port,
     output logic [3:0] b_port,
-    // SCCB side
     output logic       SIO_C,
     output logic       SIO_D,
-
-    output logic aim_detected_led,
-    output logic raser_shoot_led,
-
-    output logic target_off,
-    // SPI side
-    input  logic mosi,
-    output logic miso,
-    input  logic sclk,
-    input  logic cs
+    output logic       aim_detected_led,
+    output logic       target_off
 );
 
-
-    // global side
-    logic        sys_clk;
-
-    // VGA side
-    logic        DE;
-    logic [ 9:0] x_pixel;
-    logic [ 9:0] y_pixel;
-
-    // OV7670 side
-    logic        wclk;
-    logic        we;
-    logic [16:0] wAddr;  // [$clog2(320*240)-1:0] 
-    logic [15:0] wData;
-    logic        rclk;
-    logic        oe;
-    logic [16:0] rAddr;  // [$clog2(320*240)-1:0] 
-    logic [15:0] rData;  //imgData of IMG_MEM_READER
-
-    // img side
-    logic [ 1:0] bg_sel;
-    logic [11:0] img_cam, img_bg;
-
-    logic [9:0] aim_x, aim_y;
-    logic aim_detected;
-
-    logic [11:0] box_x_min, box_x_max, box_y_min, box_y_max;
+    logic                     sys_clk;
+    logic                     DE;
+    logic [              9:0] x_pixel;
+    logic [              9:0] y_pixel;
+    logic                     wclk;
+    logic                     we;
+    logic [             16:0] wAddr; 
+    logic [             15:0] wData; 
+    logic                     rclk;
+    logic                     oe;
+    logic [             16:0] rAddr; 
+    logic [             15:0] rData; 
+    logic [11:0] img_cam;
+    
+    // 16개 타겟 배열 신호
+    logic [15:0][9:0] aim_x_all;
+    logic [15:0][9:0] aim_y_all;
+    logic [15:0]      aim_detected_all;
+    
+    // 16개 박스 배열 신호
+    logic [15:0][11:0] x_min_all, x_max_all, y_min_all, y_max_all;
 
     // led debug
-    assign aim_detected_led = aim_detected;
-    assign raser_shoot_led = raser_shoot;
-
+    assign aim_detected_led = (aim_detected_all != 0);
     assign xclk = sys_clk;
 
-    sccb U_SCCB (
-        .clk  (clk),
-        .reset(reset),
-        .SIO_D(SIO_D),
-        .SIO_C(SIO_C)
-    );
+    // (기존 모듈들 유지)
+    sccb U_SCCB (.clk(clk), .reset(reset), .SIO_D(SIO_D), .SIO_C(SIO_C));
+    pixel_clk_gen P_CLK_GEN (.clk(clk), .reset(reset), .pclk(sys_clk));
+    VGA_Syncher U_VGA_SYNCHER (.clk(sys_clk), .reset(reset), .h_sync(h_sync), .v_sync(v_sync), .DE(DE), .x_pixel(x_pixel), .y_pixel(y_pixel));
+    image_reader U_IMG_ROM_READER (.DE(DE), .x_pixel(x_pixel), .y_pixel(y_pixel), .addr(rAddr), .imgData(rData), .r_port(img_cam[11:8]), .g_port(img_cam[7:4]), .b_port(img_cam[3:0]));
+    frame_buffer U_FRAME_BUFFER (.wclk(pclk), .we(we), .wAddr(wAddr), .wData(wData), .rclk(sys_clk), .oe(1'b1), .rAddr(rAddr), .rData(rData));
+    OV7670_controller U_OV7670_MEM_CONTROLLER (.pclk(pclk), .reset(reset), .href(href), .vsync(vsync), .data(data), .we(we), .wAddr(wAddr), .wData(wData));
 
-    pixel_clk_gen P_CLK_GEN (
-        .clk  (clk),
-        .reset(reset),
-        .pclk (sys_clk)
-    );
-
-    VGA_Syncher U_VGA_SYNCHER (
-        .clk    (sys_clk),
-        .reset  (reset),
-        .h_sync (h_sync),
-        .v_sync (v_sync),
-        .DE     (DE),
-        .x_pixel(x_pixel),
-        .y_pixel(y_pixel)
-    );
-
-    image_reader U_IMG_ROM_READER (
-        .DE     (DE),
-        .x_pixel(x_pixel),
-        .y_pixel(y_pixel),
-        .addr   (rAddr),
-        .imgData(rData),
-        .r_port (img_cam[11:8]),
-        .g_port (img_cam[7:4]),
-        .b_port (img_cam[3:0])
-    );
-
+    // [수정] Pixel Mixer 연결
     pixel_mixer U_PIXEL_MIXER (
-        .img_bg      (img_cam),
-        .aim_x       (aim_x),
-        .aim_y       (aim_y),
-        .aim_detected(aim_detected),
-        .x_pixel     (x_pixel),
-        .y_pixel     (y_pixel),
-        .box_x_min   (box_x_min),
-        .box_x_max   (box_x_max),
-        .box_y_min   (box_y_min),
-        .box_y_max   (box_y_max),
-        .r_port      (r_port),
-        .g_port      (g_port),
-        .b_port      (b_port)
+        .img_bg        (img_cam),
+        
+        .aim_x_all       (aim_x_all),
+        .aim_y_all       (aim_y_all),
+        .aim_detected_all(aim_detected_all),
+        
+        .box_x_min_all (x_min_all), .box_x_max_all (x_max_all),
+        .box_y_min_all (y_min_all), .box_y_max_all (y_max_all),
+        
+        .target_off      (target_off),
+        .x_pixel         (x_pixel),
+        .y_pixel         (y_pixel),
+        .r_port(r_port), .g_port(g_port), .b_port(b_port)
     );
 
-    frame_buffer U_FRAME_BUFFER (
-        // write side
-        .wclk (pclk),
-        .we   (we),
-        .wAddr(wAddr),  // [$clog2(320*240)-1:0] 
-        .wData(wData),
-        // read side 
-        .rclk (sys_clk),
-        .oe   (1'b1),
-        .rAddr(rAddr),  // [$clog2(320*240)-1:0] 
-        .rData(rData)
-    );
-
-    OV7670_controller U_OV7670_MEM_CONTROLLER (
-        .pclk (pclk),
-        .reset(reset),
-        // OV7670 side
-        .href (href),
-        .vsync(vsync),
-        .data (data),
-        // memory side
-        .we   (we),
-        .wAddr(wAddr),  // 320 * 240
-        .wData(wData)
-    );
-
+    // [수정] Red Tracker 연결
     red_tracker U_RED_TRACKER (
         .clk         (sys_clk),
         .reset       (reset),
@@ -149,27 +79,15 @@ module top (
         .x_pixel     (x_pixel),
         .y_pixel     (y_pixel),
         .data        (rData),
-        .aim_x       (aim_x),
-        .aim_y       (aim_y),
-        .aim_detected(aim_detected),
-        .raser_shoot (raser_shoot),
-        .x_min_out   (box_x_min),
-        .x_max_out   (box_x_max),
-        .y_min_out   (box_y_min),
-        .y_max_out   (box_y_max),
-        .target_off  (target_off)
-    );
-
-    slave_top U_SPI_Slave (
-        .clk  (clk),
-        .reset(reset),
-        .sclk (sclk),
-        .mosi (mosi),
-        .miso (miso),
-        .cs   (cs),
-        .xdata(aim_x),
-        .ydata(aim_y[8:0]),
-        .etc  (13'b0_0000_0000_0000)
+        
+        .aim_x_all       (aim_x_all),
+        .aim_y_all       (aim_y_all),
+        .aim_detected_all(aim_detected_all),
+        
+        .x_min_all(x_min_all), .x_max_all(x_max_all),
+        .y_min_all(y_min_all), .y_max_all(y_max_all),
+        
+        .target_off      (target_off)
     );
 
 endmodule
