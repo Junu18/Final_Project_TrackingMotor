f75f2153349e9d83cd1c1520f2519a77c12d933c
tmdals8184 <dltmddltmd00@naver.com>
2026-01-13 15:57:31 +0900

    structure_v7_0113_1557

---
+++ stm/structure/260113_Final/Core/ap/Listener/Listener.c
@@ -0,0 +1,16 @@
+/*
+ * Listener.c
+ *
+ *  Created on: Jan 5, 2026
+ *      Author: kccistc
+ */
+
+#include "Listener.h"
+
+void Listener_Init() {
+	Listener_Tracking_Init();
+}
+
+void Listener_Excute() {
+	Listener_Tracking_Excute();
+}
---
+++ stm/structure/260113_Final/Core/ap/Listener/Listener_Tracking.c
@@ -0,0 +1,71 @@
+/*
+ * Listener_Tracking.c
+ *
+ *  Created on: Jan 7, 2026
+ *      Author: kccistc
+ */
+
+#include "Listener_Tracking.h"
+#include "usart.h"
+#include "stm32f4xx_hal.h"
+
+hBtn hbtnStart;
+hBtn hbtnStop;
+hBtn hbtnClear;
+hBtn hbtnTargetOn;
+hBtn hbtnTargetLost;
+hBtn hbtnTargetAimed;
+hBtn hbtnDebug;
+hBtn hbtnDebugPanPlus;
+hBtn hbtnDebugPanMinus;
+
+void Listener_Tracking_Init() {
+	Button_Init(&hbtnStart, BTN_START_GPIO, BTN_START_PIN);
+	Button_Init(&hbtnStop, BTN_STOP_GPIO, BTN_STOP_PIN);
+	Button_Init(&hbtnClear, BTN_CLEAR_GPIO, BTN_CLEAR_PIN);
+	Button_Init(&hbtnTargetOn, BTN_TARGET_ON_GPIO, BTN_TARGET_ON_PIN);
+	Button_Init(&hbtnTargetLost, BTN_TARGET_LOST_GPIO, BTN_TARGET_LOST_PIN);
+	Button_Init(&hbtnTargetAimed, BTN_TARGET_AIMED_GPIO, BTN_TARGET_AIMED_PIN);
+	Button_Init(&hbtnDebug, BTN_DEBUG_GPIO, BTN_DEBUG_PIN);
+	Button_Init(&hbtnDebugPanPlus, BTN_DEBUG_PAN_PLUS_GPIO,
+			BTN_DEBUG_PAN_PLUS_PIN);
+	Button_Init(&hbtnDebugPanMinus, BTN_DEBUG_PAN_MINUS_GPIO,
+			BTN_DEBUG_PAN_MINUS_PIN);
+	SPI_Init(&hspi1);
+	SPI_RegisterCallback(Listener_OnComplete);
+}
+
+void Listener_Tracking_Excute() {
+	Listener_Tracking_CheckButton();
+}
+
+void Listener_Tracking_CheckButton() {
+	if (Button_GetState(&hbtnStart) == ACT_PUSHED) {
+		osMessagePut(trackingEventMsgBox, EVENT_START, 0);
+	} else if (Button_GetState(&hbtnStop) == ACT_PUSHED) {
+		osMessagePut(trackingEventMsgBox, EVENT_STOP, 0);
+	} else if (Button_GetState(&hbtnClear) == ACT_PUSHED) {
+		osMessagePut(trackingEventMsgBox, EVENT_CLEAR, 0);
+	} else if (Button_GetState(&hbtnTargetOn) == ACT_PUSHED) {
+		osMessagePut(trackingEventMsgBox, EVENT_TARGET_ON, 0);
+	} else if (Button_GetState(&hbtnTargetLost) == ACT_PUSHED) {
+		osMessagePut(trackingEventMsgBox, EVENT_TARGET_LOST, 0);
+	} else if (Button_GetState(&hbtnTargetAimed) == ACT_PUSHED) {
+		osMessagePut(trackingEventMsgBox, EVENT_TARGET_AIMED, 0);
+	} else if (Button_GetState(&hbtnDebug) == ACT_PUSHED) {
+		osMessagePut(trackingEventMsgBox, EVENT_DEBUG, 0);
+	} else if (Button_GetState(&hbtnDebugPanPlus) == ACT_PUSHED) {
+		osMessagePut(trackingEventMsgBox, EVENT_DEBUG_PAN_PLUS, 0);
+	} else if (Button_GetState(&hbtnDebugPanMinus) == ACT_PUSHED) {
+		osMessagePut(trackingEventMsgBox, EVENT_DEBUG_PAN_MINUS, 0);
+	}
+}
+
+void Listener_Tracking_TIM_ISR() {
+	osMessagePut(trackingEventMsgBox, EVENT_SERVO_TICK, 0);
+}
+
+void Listener_OnComplete() {
+	 HAL_UART_Transmit(&huart2, (uint8_t*)"SPI\n", 4, 10);
+	osMessagePut(trackingEventMsgBox, EVENT_FPGA_DATA_RECEIVED, 0);
+}
